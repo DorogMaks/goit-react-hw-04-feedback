91b5221302f31859bf95160c105235201222e8ea
Maksym Dorohokuplya <dorogmaks@gmail.com>
2022-10-20 17:41:48 +0300

    add step 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,57 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleIncrement = evt => {
+    const feedbackName = evt.target.name;
+    this.setState(prevState => {
+      return { [feedbackName]: prevState[feedbackName] + 1 };
+    });
+  };
+
+  render() {
+    const feedbackNames = Object.keys(this.state);
+    return (
+      <>
+        <div>
+          <h2>Please leave feedback</h2>
+          <ul>
+            {feedbackNames.map(feedbackName => {
+              return (
+                <li key={feedbackName}>
+                  <button
+                    type="button"
+                    name={feedbackName}
+                    onClick={this.handleIncrement}
+                  >
+                    {feedbackName}
+                  </button>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+
+        <div>
+          <h2>Statistics</h2>
+          <ul>
+            {feedbackNames.map(feedbackName => {
+              return (
+                <li key={feedbackName}>
+                  <p>
+                    {feedbackName}: {this.state[feedbackName]}
+                  </p>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </>
+    );
+  }
+}
