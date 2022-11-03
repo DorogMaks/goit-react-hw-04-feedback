acde8618168727e1a2c355d935db658804f91cf9
Maksym Dorohokuplya <dorogmaks@gmail.com>
2022-11-03 19:14:06 +0200

    refactoring App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react';
-import { Section } from './Section/Section';
-import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
-import { Notification } from './Notification/Notification';
-import { Statistics } from './Statistics/Statistics';
+import { Section } from '../Section/Section';
+import { FeedbackOptions } from '../FeedbackOptions/FeedbackOptions';
+import { Notification } from '../Notification/Notification';
+import { Statistics } from '../Statistics/Statistics';
 import { Wrapper } from './App.styled';
 
 export const App = () => {
